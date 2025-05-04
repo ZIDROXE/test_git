0b2cae993740321be7e9e387186860f2cff45993
abdelilah <abdelilahsadik1337@gmail.com>
2025-05-04 23:28:36 +0100

    delet

--- test.c
+++ test.c
@@ -1,5 +1,41 @@
-echo"hello world"
-file="test"
-if [ -f $file ]; then
+#include <stdio.h>
 
-echo "hello "
+// انسخ هنا الدوال ft_strlen و ft_strrev
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strrev(char *str)
+{
+	int i;
+	int len;
+	char tmp;
+
+	i = 0;
+	len = ft_strlen(str) - 1;
+	while (len > i)
+	{
+		tmp = str[i];
+		str[i] = str[len];
+		str[len] = tmp;
+		i++;
+		len--;
+	}
+	return (str);
+}
+
+int main(void)
+{
+	char str[] = "1337 School";
+	printf("Original: %s\n", str);
+	printf("Reversed: %s\n", ft_strrev(str));
+	return 0;
+}
+
